38c097c39c7a69c208ac81b351f6192860227d85
Xyq777 <140133095+Xyq777@users.noreply.github.com>
2024-10-24 00:43:30 +0800

    fixed: 优化数据库表结构

--- internal/models/message_model.go
+++ internal/models/message_model.go
@@ -5,12 +5,12 @@ import "gorm.io/gorm"
 // MessageModel 记录消息
 type MessageModel struct {
 	gorm.Model
-	SendUserID       uint      `gorm:"primaryKey" json:"send_user_id"` // 发送人id
+	SendUserID       uint      `gorm:"" json:"send_user_id"` // 发送人id
 	SendUserModel    UserModel `gorm:"foreignKey:SendUserID" json:"-"`
 	SendUserNickName string    `gorm:"size:42" json:"send_user_nick_name"`
 	SendUserAvatar   string    `json:"send_user_avatar"`
 
-	RevUserID       uint      `gorm:"primaryKey" json:"rev_user_id"` // 接收人id
+	RevUserID       uint      `gorm:"" json:"rev_user_id"` // 接收人id
 	RevUserModel    UserModel `gorm:"foreignKey:RevUserID" json:"-"`
 	RevUserNickName string    `gorm:"size:42" json:"rev_user_nick_name"`
 	RevUserAvatar   string    `json:"rev_user_avatar"`
