3b6061736c3e01fb399a642bb0b65a6d092cdcf6
Xyq777 <140133095+Xyq777@users.noreply.github.com>
2024-10-24 00:45:32 +0800

    fixed: 优化参数数据类型

--- internal/models/res/response.go
+++ internal/models/res/response.go
@@ -2,6 +2,7 @@ package res
 
 import (
 	"github.com/gin-gonic/gin"
+	"gvb/internal/global"
 	"net/http"
 )
 
@@ -11,7 +12,7 @@ type Response struct {
 	Msg  string `json:"msg"`
 }
 
-func Result(code int, data any, msg string, c *gin.Context) {
+func Result(code ErrorCode, data any, msg string, c *gin.Context) {
 	c.JSON(http.StatusOK, Response{
 		Code: code,
 		Data: data,
@@ -21,6 +22,13 @@ func Result(code int, data any, msg string, c *gin.Context) {
 func OK(data any, c *gin.Context) {
 	Result(Success, data, "操作成功", c)
 }
-func ResultWithOrigin() {
-
+func FAIL(code ErrorCode, msg string, err error, c *gin.Context) {
+	if global.Config.System.App.LogLevel == "dev" {
+		FailWithOrigin(code, msg, err, c)
+		return
+	}
+	Result(code, struct{}{}, msg, c)
+}
+func FailWithOrigin(code ErrorCode, msg string, err error, c *gin.Context) {
+	Result(code, struct{}{}, msg+err.Error(), c)
 }
