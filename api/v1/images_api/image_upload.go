4c991493c63df85ad3581b19a25d73f619dc41ce
Xyq777 <140133095+Xyq777@users.noreply.github.com>
2024-10-25 02:09:29 +0800

    feat: 添加图片进数据库并检查重复

--- api/v1/images_api/image_upload.go
+++ api/v1/images_api/image_upload.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"gvb/internal/global"
+	"gvb/internal/models"
 	"gvb/internal/models/res"
+	"gvb/tools/Encryptor"
 	"gvb/tools/validator"
+	"io"
 	"io/fs"
 	"os"
 	"path"
@@ -34,9 +37,11 @@ var (
 const KB = 1024
 const MB = KB * KB
 
+var hasFail = false
+
 // ImagesUploadAPI 上传图片，返回图片url
 func (a ImagesApi) ImagesUploadAPI(c *gin.Context) {
-	var hasFail = false
+
 	form, err := c.MultipartForm()
 	if err != nil {
 		res.FAIL(res.InvalidParams, "参数错误", c, err)
@@ -63,47 +68,78 @@ func (a ImagesApi) ImagesUploadAPI(c *gin.Context) {
 		//检查后缀
 		ext := path.Ext(fileHeader.Filename)
 		if !validator.InList(ext, WhiteImageList) {
-			resList = append(resList, FileUploadResponse{
-				FileName:  fileHeader.Filename,
-				IsSuccess: false,
-				Msg:       "图片格式错误",
-			})
-			hasFail = true
+			appendFailedResList(&resList, fileHeader.Filename, "图片格式不正确")
 			continue
 		}
 
 		//检查大小
 		size := float64(fileHeader.Size) / float64(1*MB)
 		if size >= float64(global.Config.System.Upload.Size) {
-			resList = append(resList, FileUploadResponse{
-				FileName:  fileHeader.Filename,
-				IsSuccess: false,
-				Msg:       fmt.Sprintf("图片大小超过%.2fMB,设定为%dMB", size, global.Config.System.Upload.Size),
-			})
-			hasFail = true
+			appendFailedResList(&resList, fileHeader.Filename,
+				fmt.Sprintf("图片大小超过%.2fMB,设定为%dMB", size, global.Config.System.Upload.Size),
+			)
 			continue
 		}
+		//判断图片是否入库
+		fileObj, err := fileHeader.Open()
+		if err != nil {
+			global.Log.Errorln(err)
+			appendFailedResList(&resList, fileHeader.Filename, "图片打开错误")
+			continue
+		}
+		//检查图片是否已经存在
+		fileByte, err := io.ReadAll(fileObj)
+		if err != nil {
+			global.Log.Errorln(err)
+			appendFailedResList(&resList, fileHeader.Filename, "图片读取错误")
+			continue
+		}
+		fileHash := Encryptor.Md5(fileByte)
+		result := global.Db.Limit(1).Find(&models.BannerModel{}, "hash = ?", fileHash)
+		if result.Error != nil {
+			appendFailedResList(&resList, fileHeader.Filename, "数据库查询错误")
+			continue
+		}
+		if result.RowsAffected != 0 {
+			appendFailedResList(&resList, fileHeader.Filename, "图片已存在")
+			continue
+		}
+
 		//上传
 		filePath := path.Join(basePath, fileHeader.Filename)
 		err = c.SaveUploadedFile(fileHeader, filePath)
 		if err != nil {
-			resList = append(resList, FileUploadResponse{
-				FileName:  fileHeader.Filename,
-				IsSuccess: false,
-				Msg:       "上传失败",
-			})
-			hasFail = true
+			appendFailedResList(&resList, fileHeader.Filename, "图片保存错误")
 			global.Log.Errorln(err)
 		}
-		resList = append(resList, FileUploadResponse{
-			FileName:  filePath,
-			IsSuccess: true,
-			Msg:       "上传成功",
+		global.Db.Create(&models.BannerModel{
+			Path: filePath,
+			Hash: fileHash,
+			Name: fileHeader.Filename,
 		})
+		appendSuccessResList(&resList, filePath, "图片上传成功")
+
 	}
+
 	if hasFail {
+		fmt.Print(resList)
 		res.FAIL(res.InvalidParams, "图片上传错误", c, resList)
 		return
 	}
 	res.OK(resList, c)
+}
+func appendFailedResList(resList *[]FileUploadResponse, fileName string, msg string) {
+	hasFail = true
+	*resList = append(*resList, FileUploadResponse{
+		FileName:  fileName,
+		IsSuccess: false,
+		Msg:       msg,
+	})
+}
+func appendSuccessResList(resList *[]FileUploadResponse, fileName string, msg string) {
+	*resList = append(*resList, FileUploadResponse{
+		FileName:  fileName,
+		IsSuccess: true,
+		Msg:       msg,
+	})
 }
