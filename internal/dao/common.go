165ec4296d70ad23829062418903e1a53fbf1b09
Xyq777 <140133095+Xyq777@users.noreply.github.com>
2024-10-25 23:52:21 +0800

    feat: 封装数据库操作
    content: 对find函数封装，使它返回error和count，同时使用泛型降低代码耦合度

--- internal/dao/common.go
+++ internal/dao/common.go
@@ -1,6 +1,7 @@
 package dao
 
 import (
+	"gorm.io/gorm"
 	"gvb/internal/global"
 	"gvb/internal/models/req"
 )
@@ -13,8 +14,17 @@ func GetList[T any](model T, page *req.Page) (list []T, count int, err error) {
 	if page.Sort == "" {
 		page.Sort = "created_at desc"
 	}
-	result := global.Db.Limit(page.Limit).Order(page.Sort).Offset(offset).Find(&list)
-	err = result.Error
-	count = int(result.RowsAffected)
+	res := global.Db.Limit(page.Limit).Order(page.Sort).Offset(offset).Find(&list)
+	count, err = getErrorAndCount(res)
 	return list, count, err
+}
+func FindWithIDs[T any](model T, IDList []uint) (list []T, count int, err error) {
+	res := global.Db.Find(&list, IDList)
+	count, err = getErrorAndCount(res)
+	return list, count, err
+
+}
+func getErrorAndCount(res *gorm.DB) (int, error) {
+	return int(res.RowsAffected), res.Error
+
 }
