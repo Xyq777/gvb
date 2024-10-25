6d25e80a91caea2b06f1dd5c26c33e425658f395
Xyq777 <140133095+Xyq777@users.noreply.github.com>
2024-10-25 23:53:23 +0800

    feat: 添加错误码

--- internal/models/res/err_code.go
+++ internal/models/res/err_code.go
@@ -7,7 +7,9 @@ const (
 	InvalidParams              ErrorCode = 40000
 	NotFound                   ErrorCode = 40004
 	UploadFileSizeExceedsLimit ErrorCode = 40005
+	NotFoundImages             ErrorCode = 40006
 	FailedRewriteToml          ErrorCode = 50000
 	UploadFileFailed           ErrorCode = 50001
 	FailedGetImageList         ErrorCode = 50002
+	FailedDeleteImages         ErrorCode = 50003
 )
