b55f11c09c9d1439eb469b4d5a1d9e55b13ffb4a
Xyq777 <140133095+Xyq777@users.noreply.github.com>
2024-11-01 23:25:19 +0800

    feat: 添加错误码映射msg函数

--- internal/models/res/err_code.go
+++ internal/models/res/err_code.go
@@ -13,4 +13,17 @@ const (
 	FailedGetImageList         ErrorCode = 50002
 	FailedDeleteImages         ErrorCode = 50003
 	FailedCreateDir            ErrorCode = 50004
+	DatabaseFailedCreate       ErrorCode = iota
 )
+
+var codeToMsg = map[ErrorCode]string{
+	InvalidParams:        "参数错误",
+	DatabaseFailedCreate: "数据库创建操作失败",
+}
+
+func ErrorMsg(code ErrorCode) string {
+	return codeToMsg[code]
+}
+func CodeAndMsg(code ErrorCode) (ErrorCode, string) {
+	return code, codeToMsg[code]
+}
