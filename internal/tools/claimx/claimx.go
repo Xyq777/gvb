232f504442758842c4a0df6a2cb7f647d5c70b76
Xyq777 <140133095+Xyq777@users.noreply.github.com>
2024-11-06 15:27:11 +0800

    feat: 重写提取claim方法，添加错误信息

--- internal/tools/claimx/claimx.go
+++ internal/tools/claimx/claimx.go
@@ -1,17 +1,18 @@
 package claimx
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
 	"gvb/internal/global"
 	"gvb/tools/jwt"
 )
 
-func GetClaim(c *gin.Context) *jwt.CustomClaims {
+func GetClaim(c *gin.Context) (*jwt.CustomClaims, error) {
 	_claims, ok := c.Get("claims")
 	if !ok {
 		global.Log.Error("未设置claims")
-		return nil
+		return nil, errors.New("未设置claims")
 	}
 	claims := _claims.(*jwt.CustomClaims)
-	return claims
+	return claims, nil
 }
