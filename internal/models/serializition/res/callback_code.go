8e9fee6fc2a8e95c866a45812017d05e31aaa7aa
Xyq777 <140133095+Xyq777@users.noreply.github.com>
2024-11-06 12:16:01 +0800

    feat: 添加错误码

--- internal/models/serializition/res/callback_code.go
+++ internal/models/serializition/res/callback_code.go
@@ -5,9 +5,12 @@ type ErrorCode = int
 const (
 	Success                    ErrorCode = 20000
 	InvalidParams              ErrorCode = 40000
+	AuthFailed                 ErrorCode = 40001
 	NotFound                   ErrorCode = 40004
 	UploadFileSizeExceedsLimit ErrorCode = 40005
 	NotFoundImages             ErrorCode = 40006
+	UserNotExist               ErrorCode = 40007
+	PasswordNotMatched         ErrorCode = 40008
 	FailedRewriteToml          ErrorCode = 50000
 	UploadFileFailed           ErrorCode = 50001
 	FailedGetImageList         ErrorCode = 50002
@@ -16,15 +19,20 @@ const (
 	DatabaseOperateError       ErrorCode = 50005
 	DatabaseFailedCreate       ErrorCode = 50006
 	DatabaseMenuFailedDelete   ErrorCode = 50007
+	TokenGenerateFailed        ErrorCode = 50008
 )
 
 var codeToMsg = map[ErrorCode]string{
 	Success:                  "操作成功",
 	NotFound:                 "资源不存在",
 	InvalidParams:            "参数错误",
+	UserNotExist:             "用户不存在",
+	AuthFailed:               "认证失败",
+	PasswordNotMatched:       "密码错误",
 	DatabaseFailedCreate:     "数据库创建操作失败",
 	DatabaseOperateError:     "数据库操作失败",
 	DatabaseMenuFailedDelete: "数据库菜单删除失败",
+	TokenGenerateFailed:      "Token生成失败",
 }
 
 func CodeMsg(code ErrorCode) string {
