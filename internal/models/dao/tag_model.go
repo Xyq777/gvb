ce154ea763c7ecce492d6690ec32c5848d8be193
Xyq777 <140133095+Xyq777@users.noreply.github.com>
2024-11-23 20:48:51 +0800

    fix: change method receiver type

--- internal/models/dao/tag_model.go
+++ internal/models/dao/tag_model.go
@@ -8,19 +8,19 @@ type TagModel struct {
 	Title string `gorm:"size:16;comment:标签的名称;unique" json:"title"` // 标签的名称
 }
 
-func (t TagModel) FindWithTitle(tx *gorm.DB) (bool, error) {
+func (t *TagModel) FindWithTitle(tx *gorm.DB) (bool, error) {
 	result := tx.Find(&t).Where("title = ?", t.Title)
 	if result.RowsAffected == 0 {
 		return false, result.Error
 	}
 	return true, result.Error
 }
-func (t TagModel) Create(tx *gorm.DB) error {
+func (t *TagModel) Create(tx *gorm.DB) error {
 	return tx.Create(t).Error
 }
-func (t TagModel) Update(tx *gorm.DB) error {
+func (t *TagModel) Update(tx *gorm.DB) error {
 	return tx.Updates(t).Where("tittle = ?", t.Title).Error
 }
-func (t TagModel) Delete(tx *gorm.DB) error {
+func (t *TagModel) Delete(tx *gorm.DB) error {
 	return tx.Model(t).Where("tittle = ?", t.Title).Delete(&t).Error
 }
